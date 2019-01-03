5bb13f97dabb1d7abce4a254febbcb36768ee20e
gaosong <805940564@qq.com>
2019-01-03 22:30:43 +0800

    add log

--- springbean-circular-dependency/src/test/java/com/gsonkeno/circular/dependency/StudentBootStrap.java
+++ springbean-circular-dependency/src/test/java/com/gsonkeno/circular/dependency/StudentBootStrap.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class StudentBootStrap {
 
     public static void main(String[] args) {
+        // 构造器循环依赖，启动报错
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("student_beans.xml");
     }
 }
